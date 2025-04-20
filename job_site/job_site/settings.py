ad6fc6e2275add846b52a629eef8b94ea3e00565
Haruto07 <haruto.yunawan@gmail.com>
2025-04-20 02:27:19 -0700

    dsa

--- job_site/job_site/settings.py
+++ job_site/job_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # Django REST framework for API development
+    'reentry_jobs', # Your app for job listings and seekers
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth.User'
+LOGIN_REDIRECT_URL = 'profile_type'
+LOGOUT_REDIRECT_URL = 'home'
